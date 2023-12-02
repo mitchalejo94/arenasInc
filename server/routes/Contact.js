d29fbad108a3024ed12acbc0a9bf5381d566e696
Mitchel Alejo <michalejo94@gmail.com>
2023-12-02 14:57:45 -0600

    Feat : get contact submission by id route

--- server/routes/Contact.js
+++ server/routes/Contact.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const { Contact } = require("../models");
 
 //Routers
-//GET = main user
+//GET All = main user
 router.get("/", async (req, res) => {
   const contactList = await Contact.findAll();
   res.json(contactList);
+});
+
+//Get by id//
+router.get("/:contactId", async (req, res) => {
+  const id = req.params.contactId;
+  const contactId = await Contact.findByPk(id);
+  res.json(contactId);
 });
 
 //POST - anyone
@@ -14,6 +21,17 @@ router.post("/", async (req, res) => {
   const commentPost = req.body;
   await Contact.create(commentPost);
   res.json(commentPost);
+});
+
+//Delete Route
+router.delete("/:contactId", async (req, res) => {
+  const contactId = req.body.contactId;
+  await Contact.destroy({
+    where: {
+      id: contactId,
+    },
+  });
+  res.json("deleted success");
 });
 
 module.exports = router;
