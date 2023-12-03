a5650cbe1e6c89c5930d323689dd9e78f23e2768
Mitchel Alejo <michalejo94@gmail.com>
2023-12-02 21:54:15 -0600

    Feat: Added front end client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Contact from "./components/Contact";
 import ContactForm from "./components/ContactForm";
+import Login from "./components/Login";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/contactList" element={<Contact />}></Route>
           <Route path="/contactForm" element={<ContactForm />}></Route>
+          <Route path="/adminUsers/login" element={<Login />}></Route>
         </Routes>
       </Router>
     </>
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const login = () => {
+    const data = { username: username, password: password };
+    axios
+      .post("http://localhost:3003/adminUsers/login", data)
+      .then((response) => {
+        console.log(response.data);
+      });
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <input
+        type="password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
