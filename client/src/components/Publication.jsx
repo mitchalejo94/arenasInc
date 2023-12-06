5a56bb7ebdc1b14107ec3a41011b8fffca82430f
Mitchel Alejo <michalejo94@gmail.com>
2023-12-05 20:02:13 -0600

    Feat: Added individual publication page in frontend

--- client/src/components/Publication.jsx
+++ client/src/components/Publication.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 function Publication() {
   let { id } = useParams();
-  return <div>Publication ID: {id}</div>;
+  const [contactText, setContactText] = useState({});
+
+  useEffect(() => {
+    axios.get(`http://localhost:3003/Contact/${id}`).then((response) => {
+      console.log(response.data);
+      setContactText(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Publication ID: {id}</h1>
+      <div>
+        <div>{contactText.name}</div>
+        <div>{contactText.email}</div>
+        <div>{contactText.message}</div>
+        <div>{contactText.phoneNumber}</div>
+        <div>{contactText.cityState}</div>
+      </div>
+    </>
+  );
 }
 
 export default Publication;
